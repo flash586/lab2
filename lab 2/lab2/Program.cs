b80cf8236bdc7d847a47728d3ad8df69806296e9
flash586 <ironpatriot864@gmail.com>
2019-06-09 01:07:44 +0300

    Initial commit

---
+++ lab 2/lab2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace lab2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix();
+            matrix.Y_3();
+            matrix.b_();
+            matrix.y1_();
+            matrix.y2_();
+   
+            Thread first = new Thread(new ThreadStart(matrix.first_));
+            Thread second = new Thread(new ThreadStart(matrix.second_));
+            Thread third = new Thread(new ThreadStart(matrix.third_));
+            Thread fourth = new Thread(new ThreadStart(matrix.fourth_));
+            Thread fifth = new Thread(new ThreadStart(matrix.fifth_));
+            first.Start();
+            first.Join();
+            second.Start();
+            second.Join();
+            third.Start();
+            third.Join();
+            fourth.Start();
+            fourth.Join();
+            fifth.Start();
+            fifth.Join();
+            Thread x = new Thread(new ThreadStart(matrix.x));
+            x.Start();
+            x.Join();
+            matrix.Output();
+            Console.ReadKey();
+        }
+    }
+}
